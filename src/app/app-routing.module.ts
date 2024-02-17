66349a37ff8fcd34e623322cce3905bd226b86dc
Diego Nunes <diegodnunes12@gmail.com>
2024-02-17 16:46:30 -0300

    criando a estrutura do projeto com as rotas e os dois módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "",
+        loadChildren: () => import("./store/store.module").then(storeModule => storeModule.StoreModule),
+      },
+      {
+        path: "profile",
+        loadChildren: () => import("./customer/customer.module").then(customerModule => customerModule.CustomerModule),
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerComponent } from './customer.component';
+import { CustomerRoutingModule } from './customer-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CustomerRoutingModule
+  ],
+  declarations: [CustomerComponent]
+})
+export class CustomerModule { }
