aa4b358e7d91fefd69c768816c0aa7906f6270bc
Diego Nunes <diegodnunes12@gmail.com>
2024-02-17 17:19:27 -0300

    Criando o header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
